30f4ede47d7fb73abaa69ba04c11aa6e9db532fa
hingyenyin <yenyin.262@gmail.com>
2016-12-26 23:00:29 +0800

    almost there

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -18,11 +18,32 @@ import Foundation
  */
 // write your code here
 
+class Person {
+    
+    var firstName: String
+    
+    var lastName: String
 
+    var fullName: String {
+        get {
+        return "\(firstName) \(lastName)"
+    
+    }
+    }
+    
+    init(firstName: String, lastName: String) {
+       self.firstName = firstName
+        
+    self.lastName = lastName
+        
+    }
+    
+    func greet(_ anotherPerson: Person) -> String {
+        
+        return "Hello, \(person.firstName)"
 
-
-
-
+}
+}
 
 // Test
 let person = Person(firstName: "Alice", lastName: "Johnson")
@@ -84,7 +105,41 @@ extension Double {
 // write your code here
 
 
-
+class Transaction {
+    
+    var type: String
+    var amount: Double
+    
+    var description: String {
+        
+        get {
+        
+        var typeValue = type
+        
+        if typeValue == "in" {
+            
+            typeValue = "credit"
+        }
+            
+        else if  typeValue == "out" {
+            typeValue == "debit"
+            
+            }
+            else { typeValue = "null"
+            }
+        
+        return "Transaction: \(type) in the amount of $\(amount)"
+        }
+        
+    }
+    init(type: String, amount: Double) {
+        
+        self.type = type
+        self.amount = amount
+        
+    }
+    
+}
 
 
 
@@ -135,17 +190,62 @@ assert(transaction2.description == "Transaction: debit in the amount of $1.20", 
 // write your code here
 
 
+class BankAccount {
+    
+    var owner: Person
+    var transactions: [Transaction]
+    
+    var balance: Double {
+      
+        get {
+            
+        var finalBalance = 0.0
+        
+        for amount in transactions {
+            
+            if amount.type == "in" {
+                
+                finalBalance += amount.amount
+            } else {
+                
+                finalBalance -= amount.amount
+            }
+        }
+                return finalBalance
+            }
+    
+    }
+    
+    init(owner: Person) {
+        self.owner = owner
+        self.transactions = []
+        
+    }
+    
+    func deposit(_ amount: Double) {
+    
+        let transaction = Transaction(type: "in", amount: amount);
+        
+        transactions.append(transaction)
+    }
+    
+        func withdraw(_ amount: Double)
+        {
+            let transaction = Transaction(type: "out", amount: amount)
+            transactions.append(transaction)
+            
+        
+        }
 
-
-
-
+    
+}
 
 
 
 
 // Test
 let personBankAccount = BankAccount(owner: person)
-assert(personBankAccount.owner.fullName == "Alice Johnson", personBankAccount.owner.fullName)
+ assert(personBankAccount.owner.fullName == "Alice Johnson", personBankAccount.owner.fullName)
 assert(personBankAccount.transactions.isEmpty)
 
 /*: section7
