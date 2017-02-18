9348226d8154d397ae418bba1eeabf55db6108b6
ittakesii <julian@alumni.american.edu>
2017-02-17 19:10:51 -0500

    Done and tests passed

--- PersonStuff/LaLaLand.swift
+++ PersonStuff/LaLaLand.swift
@@ -9,9 +9,20 @@
 import Foundation
 
 class LaLaLand {
+    var sebastian: Person = Person(firstName: "Sebastian", lastName: "Wilder")
+    var mia: Person = Person(firstName: "Mia", lastName: "Dolan")
     
+    func initialMeeting() -> String {
+       let greetPerson = mia.greet(person: sebastian)
+        
+        return greetPerson
+    }
 
+    func dateNight() -> String {
+       let danceWith = mia.dance(with: sebastian)
     
+        return danceWith
+    }
     
     
     
