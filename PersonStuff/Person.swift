05cb20ca85705f52eda437afb9d2d76091106d70
Cynthia Nikolai <cnikolai@earthlink.net>
2018-01-11 16:48:29 -0500

    finished exercise

--- PersonStuff/Person.swift
+++ PersonStuff/Person.swift
@@ -10,8 +10,27 @@ import Foundation
 
 class Person {
     
-
+    let firstName:String
+    let lastName:String
+    var happiness:Int = 0
+    var fullName:String
     
+    init (firstName: String, lastName:String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        fullName = "\(self.firstName) \(self.lastName)"
+    }
     
+    func greet(person: Person) -> String {
+        person.happiness += 2
+        happiness += 2
+        return "Hello \(person.fullName)."
+    }
+    
+    func dance(with person: Person) -> String {
+        person.happiness += 5
+        happiness += 5
+        return "💃🏼\(fullName)❤️ ❤️\(person.fullName)💃🏼"
+    }
     
 }
