f5bc80301394d99101908e2fb6cd1fd464753d1e
Gustavo Picciuto <gustavo.picciutomaury@live.com>
2018-09-30 15:43:53 -0400

    Complete problems

--- PersonStuff/LaLaLand.swift
+++ PersonStuff/LaLaLand.swift
@@ -7,12 +7,20 @@
 //
 
 import Foundation
+import UIKit
 
 class LaLaLand {
+    //class properties
+    let sebastian: Person = Person(firstName: "Sebastian", lastName: "Wilder")
+    let mia: Person = Person(firstName: "Mia", lastName: "Dolan")
     
-
+    //class methods
+    func initialMeeting() -> String{
+        return mia.greet(person: sebastian)
+    }
     
-    
-    
+    func dateNight() -> String {
+        return mia.dance(with: sebastian)
+    }
     
 }
--- PersonStuff/Person.swift
+++ PersonStuff/Person.swift
@@ -5,13 +5,36 @@
 //  Created by Jim Campagno on 1/31/17.
 //  Copyright © 2017 Jim Campagno. All rights reserved.
 //
-
+import UIKit
 import Foundation
 
 class Person {
+    //calss properties
+    let firstName: String
+    let lastName: String
+    var happiness: Int = 0
+    var fullName: String {
+        get{
+            return "\(firstName) \(lastName)"
+        }
+    }
+    //initializer 
+    init(firstName: String, lastName: String){
+        self.firstName = firstName
+        self.lastName = lastName
+    }
     
-
+    //Class methods
+    func greet(person: Person) -> String {
+        self.happiness += 2
+        person.happiness += 2
+        return "Hello \(person.fullName)."
+    }
     
-    
-    
+    func dance(with person:Person) -> String{
+        self.happiness += 5
+        person.happiness += 5
+        return "💃🏼\(self.fullName)❤️ ❤️\(person.fullName)💃🏼"
+        
+    }
 }
