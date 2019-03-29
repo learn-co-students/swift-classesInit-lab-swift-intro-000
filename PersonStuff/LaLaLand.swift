e03e1da0df312f10005780e578b67cb21e36e897
Orvybr0wn1 <orvyb1@gmail.com>
2019-03-28 21:53:12 -0400

    Swift Classes Init

--- PersonStuff/LaLaLand.swift
+++ PersonStuff/LaLaLand.swift
@@ -8,11 +8,20 @@
 
 import Foundation
 
-class LaLaLand {
-    
+class LaLaLand
+{
+    var sebastian: Person = Person(firstName: "Sebastian", lastName: "Wilder")
 
+    var mia: Person = Person(firstName: "Mia", lastName: "Dolan")
     
     
+    func initialMeeting() -> String
+    {
+        return mia.greet(person: sebastian)
+    }
     
-    
+    func dateNight() -> String
+    {
+        return mia.dance(with: sebastian)
+    }
 }
